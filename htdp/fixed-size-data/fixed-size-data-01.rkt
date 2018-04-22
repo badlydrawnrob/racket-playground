21b6287365ea01df6e07c2c417e7ea7527e5833d
Rob Mcdonald <hello@robmcdonald.me>
2018-04-22 20:27:38 +0100

    Starting next chapter

---
+++ htdp/fixed-size-data/fixed-size-data-01.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fixed-size-data-01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(/ 2 3 4)
+(max 1 3)
+pi
+(/ 4 6)
+(define x 12)
+(define y 5)
+(sqrt (+ (* x x) (* y y)))
