2fb259bca650df4a690b6f6d78a8f0dfbf326821
Rob Mcdonald <hello@robmcdonald.me>
2018-04-24 18:37:52 +0100

    A few more exercises on arithmetic

---
+++ htdp/fixed-size-data/fixed-size-data-02.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fixed-size-data-02) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 1.5 Arithmetic of Booleans
+
+
+
