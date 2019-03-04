e1f1fb1370397b054927682194c59a8825a3ce05
Rob McDonald <hello@robmcdonald.me>
2019-03-04 21:40:03 +0000

    Update fixed size data exercises

--- htdp/fixed-size-data/fixed-size-data-08.rkt
+++ htdp/fixed-size-data/fixed-size-data-08.rkt
@@ -7,15 +7,24 @@
 ;; 2. Chunk it into component parts (questions/output)
 ;; 3. Create one function per task
 ;; 4. Simplify! KISS, ACID, DRY, IO (no mutation)
+;;
+;; "For every constant mentioned in a problem statement,
+;;  introduce one constant definition."
 
 ;; Ex.27: REDUCE, Reduce, reduce!
 ;; - see `fixed-size-data-07`
 
-;; Set constants:
+;; Set constants
+;; — Name of a constant is a "global variable"
+;; - Constant definition (left-hand side)
+;; - Literal constant or expression (right-hand side)
+;; - GLOBAL-VARIABLES (should be) uppercase
 (define std-ticket-price 5.0)  ;; At this cost ...
 (define std-attendance 120)    ;; This many people attend
 (define var-price-change 0.1)  ;; For each 10-cent change ...
 (define var-attendance 15)     ;; +/- 15 people difference
+(define PRICE-SENSITIVITY
+  (/ var-attendance var-price-change))
 (define cost-per-head 0.04)
 (define cost-set 180)
 
@@ -23,7 +32,7 @@
 (define (attendees ticket-price)
   (- std-attendance
      (* (- ticket-price std-ticket-price)
-        (/ var-attendance var-price-change))))
+        PRICE-SENSITIVITY)))
 
 ;; #2
 (define (revenue ticket-price)
@@ -37,8 +46,7 @@
 (define (profit ticket-price)
   (- (revenue ticket-price)
      (cost ticket-price)))
-
-;; 
+ 
 
 ;; Ex.28
 ;; Find potential profit:
@@ -48,3 +56,27 @@
  (profit 3)  ;; $3
  (profit 4)  ;; $4
  (profit 5)) ;; $5
+
+;; Ex.29
+;; Increase variable cost
+;; Reduce fixed cost
+;; Test prices (results in lower profits?)
+
+(define revised-cost-per-head 1.50)
+(define (revised-cost ticket-price)
+  (* revised-cost-per-head (attendees ticket-price)))
+
+(define (revised-profit ticket-price)
+  (- (revenue ticket-price)
+     (revised-cost ticket-price)))
+
+(list
+ (revised-profit 3)
+ (revised-profit 4)
+ (revised-profit 5))
+
+;; Ex. 30
+;; Add a constant for the price sensitivity
+;; Add to previous functions
+;;
+;; See: PRICE-SENSITIVITY
---
+++ htdp/fixed-size-data/fixed-size-data-09.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fixed-size-data-09) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 2.5 Programs
+;;
+;; 1. What is the problem question?
+;; 2. Chunk it into component parts (questions/output)
+;; 3. Create one function per task
+;; 4. Simplify! KISS, ACID, DRY, IO (no mutation)
+;;
+;; "For every constant mentioned in a problem statement,
+;;  introduce one constant definition."
+;;
+;; Two types of programs: Batch (i/o), interactive (gui)
+
+(require 2htdp/batch-io)
+
+;; (write-file filename string)
+;; - also accepts 'stdout (prints string to console)
+
+;; (read-file string)
+
