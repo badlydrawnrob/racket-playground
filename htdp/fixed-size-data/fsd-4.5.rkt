89e84215c9a0bec53defa08d531b7d51eb6b0684
Rob McDonald <hello@robmcdonald.me>
2019-05-06 16:34:00 +0100

    Update and start splitting out

--- htdp/fixed-size-data/fsd-4.5.rkt
+++ htdp/fixed-size-data/fsd-4.5.rkt
@@ -8,6 +8,11 @@
 ;; for instance, it could be
 ;; - A boolean type (only #t or #f)
 ;; - A number (infinite possibilities)
+;;
+;; != When checking different data types, be sure to
+;;    check if it's an actual type (if you just use `string=?`,
+;;    and try and pass a Number, it'll throw an error!
+;;    - See: `(show x)`
 
 (require 2htdp/batch-io)
 (require 2htdp/universe)
@@ -29,10 +34,21 @@
     [(false? x) 3]
     [else (+ x 3)]))
 
-;; Sample problem
-;; --------------
+;; Exercise 53, 54, 55
+;; -------------------
 ;; Design a program that launches a rocket
 ;; + When user presses space bar
+;;
+;; - "resting"
+;; - a Number between -3 and -1
+;; - a NonnegativeNumber
+;; interpretation a grounded rocket, in countdown mode,
+;; a number denotes the number of pixels between the
+;; top of the canvas and the rocket (its height)
+;;
+;; 1. Draw out the states
+;; 2. Write out a wishlist
+;; 3. Use the correct conditions for each sub-class of type
 
 (define HEIGHT 300) ; distances in pixels 
 (define WIDTH  100)
@@ -42,45 +58,85 @@
 (define ROCKET (rectangle 5 30 "solid" "red"))
  
 (define CENTER (/ (image-height ROCKET) 2))
+(define BOTTOM (- HEIGHT CENTER))
+    
 
-; Distance -> Distance
-; Decrease the distance by 3 pixels
-; — Decreases distance from top of canvas
-(check-expect (distance (- HEIGHT CENTER)) (- HEIGHT CENTER))
-(check-expect (distance 30) 27)
-(define (distance y)
+; Number/String -> Boolean
+; Is it time to countdown?
+(check-expect (countdown? "resting") #false)
+(check-expect (countdown? -2) #true)
+(define (countdown? x)
   (cond
-    [(= y (- HEIGHT CENTER)) (- HEIGHT CENTER)]
-    [else y]))
+    [(and (number? x)
+          (<= -3 x -1)) #true]
+    [else #false]))
 
-; WorldState -> WorldState
-; Render the rocket
-(define (rocket y)
-  (place-image
-   ROCKET (/ WIDTH 2) y BACKG))
-
-; Image -> Image
-; Resting rocket
-
-; Image -> Image
-; Launching rocket
+; LRCD -> Image
+; Split out the place-image function
+; - DRY!
 
 
-; ... -> ...
-; - "resting"
-; - NonnegativeNumber
-; interpretation "resting" represents a grounded rocket
-; a number denotes the height of a rocket in flight
-(define (rocket/launch y a-key)
+; LRCD -> Image
+; renders the state as a resting or flying rocket
+; #3
+(check-expect (show "resting")
+              (place-image ROCKET (/ WIDTH 2) BOTTOM BACKG))
+(check-expect (show -2)
+              (place-image
+               (text "-2" 20 "red")
+               10 (* 3/4 WIDTH)
+               (place-image ROCKET (/ WIDTH 2) BOTTOM BACKG)))
+(check-expect (show 0)
+              (place-image ROCKET (/ WIDTH 2) 0 BACKG))
+(check-expect (show 52)
+              (place-image ROCKET (/ WIDTH 2) 52 BACKG))
+
+
+(define (show x)
+   (cond
+     [(and (string? x)
+           (string=? "resting" x))
+      (place-image ROCKET (/ WIDTH 2) BOTTOM BACKG)]
+     [(<= -3 x -1)
+      (place-image (text (number->string x) 20 "red")
+                   10 (* 3/4 WIDTH)
+                   (place-image ROCKET
+                                (/ WIDTH 2) BOTTOM
+                                BACKG))]
+      [(>= x 0)
+       (place-image ROCKET (/ WIDTH 2) x BACKG)]))
+
+; LRCD KeyEvent -> LRCD
+; starts the countdown when space bar is pressed,
+; if the rocket is still resting
+(check-expect (launch -2 " ") -2)
+(define (launch x ke)
   (cond
-    [(key=? a-key " ") (- y YDELTA)]
-    [else y]))
+    [(key=? ke " ") (show -3)]
+    [else (show x)]))
+  
+
+; LRCD -> LRCD
+; raises the rocket by YDELTA,
+; — only if it's moving already
+;(check-expect (fly "resting") "resting")
+;(check-expect (fly -2) -2)
+;(check-expect (fly -1) 0)
+;(check-expect (fly 250) 247)
+
+;(define (fly x)
+;  (cond
+;    [(string?) x]
+;    [(<= -3 x -1)
+;     (if (= x -1) BOTTOM (+ x 1))]
+;    [(>= x 0) (- x YDELTA)]))
+    
 
 ; WorldState -> WorldState
 ; Starts the program
 ; - #true or #false
-(define (main y)
-  (big-bang y
-    [on-tick distance]
-    [to-draw rocket]
-    [on-key rocket/launch]))
+;(define (main y)
+;  (big-bang y
+;    [on-tick fly]
+;    [to-draw show]
+;    [on-key launch]))
