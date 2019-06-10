0da4dd14a59736e5cda804139a52d5dbefbf1351
--global <hello@robmcdonald.me>
2019-06-10 12:03:37 +0800

    Change tests, Add templates for exercise
    - Use tests from github
    - One template also includes two data types

--- htdp/fixed-size-data/fsd-5.8.rkt
+++ htdp/fixed-size-data/fsd-5.8.rkt
@@ -26,15 +26,17 @@
 (require 2htdp/image)
 
 
+;; Sample problem
+;; --------------
 
 
 (define-struct r3 [x y z])
-;; An R3 is a structure:
-;;    (make-r3 Number Number Number)
-;; interpretation:
-;;    Distance of objects in a
-;;    3-dimensional space to origin
-;;    using the x, y, z (depth?) coordinates
+; An R3 is a structure:
+;    (make-r3 Number Number Number)
+; interpretation:
+;    Distance of objects in a
+;    3-dimensional space to origin
+;    using the x, y, z (depth?) coordinates
  
 (define ex1 (make-r3 1 2 13))
 (define ex2 (make-r3 -1 0 3))
@@ -52,6 +54,66 @@
            (sqr (r3-z p)))))    ; #1
 
 
-(check-within 13.19 (r3-distance-to-0 ex1) 13.20)  ; #2
-(check-within 3.162 (r3-distance-to-0 ex2) 3.163)  ; #2
+(check-within (r3-distance-to-0 (make-r3 0 0 0)) 0 0.01)           ; #2
+(check-within (inexact->exact (r3-distance-to-0 ex1)) 13.19 0.01)  ; #2
+(check-within (inexact->exact (r3-distance-to-0 ex2)) 3.163 0.01)  ; #2
 
+
+
+
+
+;; Exercise 80
+;; ===========
+
+(define-struct movie [title director year])
+; A Movie is a structure:
+;   (make-movie String String Number)
+; interpretation:
+;   A movie with a title, director and year of release
+
+; Movie -> String
+; outputs the movie information
+(define (output-movie m)
+  (... (movie-title m) ... (movie-director ... (movie-year) ...)))
+
+
+(define-struct pet [name number])
+; A Pet is a structure:
+;   (make-pet String Number)
+; interpretation:
+;   Generates an instance of pet,
+;   with a name and id number
+
+; Pet -> String
+; outputs the pet information
+(define (output-pet p)
+  (... (pet-name p) ... (pet-year) ...))
+
+
+(define-struct CD [artist title price])
+; A CD is a structure:
+;   (make-pet String String Number)
+; interpretation:
+;   An instance of a CD with the artist, title and price
+
+; CD -> Number
+; outputs the price of the CD
+(define (output-cd cd)
+  (... (cd-artist cd) ... (cd-title cd) ... (cd-price cd) ...))
+
+
+(define-struct sweater [material size color])
+; A Sweater is (make-sweater String Size Color)
+; interpretation:
+;    An instance of a sweater, with it's
+;    material size (type) and color (type)
+
+; A Size is one of:
+; - "small"
+; - "medium"
+; - "large"
+
+; A Color is one of:
+; - "black"
+; - "white"
+; - "blue"
