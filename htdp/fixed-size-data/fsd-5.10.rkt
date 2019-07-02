141430271c185347b5695448ff68f04ea766f28c
Rob <hello@robmcdonald.me>
2019-07-02 21:43:55 +0100

    Fix tabs and returns
    Order seems to matter in the conditional statements

--- htdp/fixed-size-data/fsd-5.10.rkt
+++ htdp/fixed-size-data/fsd-5.10.rkt
@@ -80,8 +80,8 @@
     [(key=? "left" ke) (move-left ed)]
     [(key=? "right" ke) (move-right ed)]
     [(key=? "\b" ke) (del-char ed)] ; delete char to left of cursor (if any)
+    [(or (key=? "\t" ke) (key=? "\r" ke)) ed] ; ignore tab or return
     [(= (string-length ke) 1) (add-char ed ke)] ; add any single char
-    [(or (key=? "\t" ke) (key=? "\r")) ed] ; ignore tab or return
     [else ed])) ; ignore other KeyEvents
 
 
