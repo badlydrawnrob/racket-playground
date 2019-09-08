1562e2e288e25a677e9283d7425df4d768835b6e
Rob <hello@robmcdonald.me>
2019-09-08 13:08:01 +0100

    Rename again

---
+++ htdp/fixed-size-data/fsd-6.2.3.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fsd-6.2.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 6.2 Mixing up worlds
+;; ====================
+;;
+
+(require 2htdp/universe)
+(require 2htdp/image)
+
+
+;; Exercise 108
+;; ------------
+
