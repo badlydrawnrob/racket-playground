837a75e21fd33aaa1d805abe8254bdc7d55e9679
Rob <hello@robmcdonald.me>
2019-09-19 21:24:48 +0100

    Update working function

--- beautiful-racket/recursion-02.rkt
+++ beautiful-racket/recursion-02.rkt
@@ -16,14 +16,29 @@
 ;;;;    to a list properly within the function, so make it
 ;;;;    easier by declaring a variable as a list.
 ;;;;
-;;;; #: You actually may not even need a list for this
+;;;; # A string is actually a fixed-length array of characters!
+;;;;
+;;;;   @link: https://docs.racket-lang.org/reference/strings.html
+;;;;   @link: https://bit.ly/2m3eS5i
+;;;;   @link: https://realpython.com/python-strings/
+;;;;
+;;;; #1: You actually may not even need a list for this
+;;;;     but I'm not sure how yet!
+;;;;
+;;;; #2: (string->list ...) creates a list of chars (e.g: #\s)
+;;;;     (list "s" "t") is two 1Char strings (less explicit)
+;;;;
+;;;;     - Meaning, you have to convert (#2a) back into a string
+;;;;     - You could probably also use (string-join ...) here too
+;;;;     - Remember to read functions inside->out
 
 
 ;;; Adding hyphens to a string
 ;;; --------------------------
 ;;; You need to convert a string to a list, check if a string
-;;; is empty, and add a hyphen on each character, expect the first
-;;; (if there's only one char) or the last of a series of chars
+;;; is empty, and add a hyphen on each character.
+;;; If there's only one char, no hyphen
+;;; If the last of a series of chars, no hyphen
 
 
 ;;; Wish list
@@ -36,20 +51,33 @@
 ;;;   - convert to a list
 ;;;   - have a base condition to stop an infinite loop
 
-(define EMPTY (string->list ""))
-(define S (string->list "s"))
-(define STRING (string->list "string"))
-
 
 ; String -> String
-; Convert a string to a hyphenated string
+; for this method you have to slowly remove one letter
+; from the string as you pass the remainder back into the function
 (define (string->hyphens str)
   (cond
-    [(empty? str) (string-append "" "")]
-    [(= 1 (length str)) (string-append (list->string str))]
-    [(> 1 (length str)) (string-append (list->string (first str)) "-")]
-    [else (string->hyphens (rest str))]))
+    [(< (string-length str) 1) ""]
+    [(= (string-length str) 1) str]
+    [else ...])) ; #1
 
-(check-expect (string->hyphens EMPTY) "")
-(check-expect (string->hyphens S) "s")
-(check-expect (string->hyphens STRING) "s-t-r-i-n-g")
+
+; A List of Strings -> String
+; for this method you have to have the initial empty? and cons?
+; conditions, with an extra if condition for the last (single) char.
+(define (alos->hyphens ls)
+  (cond
+    [(empty? ls) ""]
+    [(cons? ls)
+     (if (= 1 (length ls))
+         (string (first ls))
+         (string-append (string (first ls)) "-"
+                    (alos->hyphens (rest ls))))]))
+
+(define EMPTY (string->list ""))        ; #2
+(define S (string->list "s"))           ; #2
+(define STRING (string->list "string")) ; #2
+
+(check-expect (alos->hyphens EMPTY) "")
+(check-expect (alos->hyphens S) "s")
+(check-expect (alos->hyphens STRING) "s-t-r-i-n-g")
