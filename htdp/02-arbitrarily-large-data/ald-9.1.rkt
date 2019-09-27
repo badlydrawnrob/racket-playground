35ab670f76f0784320991320d326a049f498cbab
Rob <hello@robmcdonald.me>
2019-09-27 18:02:04 +0100

    Add another exercise
    The base case is interesting as the value we're looking for isn't there, but it needs to be #true for the function to work properly

--- htdp/02-arbitrarily-large-data/ald-9.1.rkt
+++ htdp/02-arbitrarily-large-data/ald-9.1.rkt
@@ -4,6 +4,17 @@
 ;;;; 9.1 Finger Exercises: Lists
 ;;;; ===========================
 ;;;;
+;;;; ##: REVISIT input errors (6.3 Input Errors)
+;;;;     and validating a list before passing it to a "proper"
+;;;;     function (ex. 139 below)
+;;;;
+;;;; ##: Pay attention to base case (empty list) and booleans
+;;;;
+;;;;     - see #4 and #5
+;;;;     - for now, we set '() to #true even if the function is
+;;;;       looking for a specific value (i.e boolean value)
+;;;;
+;;;;
 ;;;; #1: You could add a (positive? ...) check here,
 ;;;;     but you'd need to add an error and possibly a nested
 ;;;;     condition ...
@@ -12,9 +23,26 @@
 ;;;;     sub-classes of allowed data types
 ;;;;
 ;;;;     - You'd use (and ...) here as ALL need to be #true
+;;;;     - You could also use the (positive? ...) function
 ;;;;
-;;;; #3: This works, but everytime the function runs it get's
-;;;;     passed through the (pos? ...) function, inefficient
+;;;; #3: First we pass the entire list to the pos function,
+;;;;     if #true, we can pass it forward to the original sum function.
+;;;;     We're separated concerns by having:
+;;;;
+;;;;     1. A recursive sum function
+;;;;     2. A recursive pos? function
+;;;;     3. A helper function to check for errors, then pass list
+;;;;        to the sum function
+;;;;
+;;;;     #: This works, but everytime the function runs it get's
+;;;;        passed through the (pos? ...) function, inefficient
+;;;;
+;;;; #4: The value for '() seems to need to be #true, otherwise it returns
+;;;;     the wrong values (it returns #false when empty, breaking function)
+;;;;
+;;;; #5: You'd actually think this base case would confuse the function by
+;;;;     always returning #true (as eventually the list is empty) but it
+;;;;     doesn't seem to.
 
 
 ;;; Exercise 137
@@ -27,7 +55,7 @@
 ;;; - a base case
 ;;; - and selectors
 ;;;
-;;; The implementation is different:
+;;; The implementation is different, mainly the combining function:
 ;;;
 ;;; - one uses cons? One uses else
 ;;; - one uses OR, one uses +
@@ -140,14 +168,81 @@
 ; (if one of List-of-Amounts) otherwise returns an error
 (define (checked-sum l)
   (cond
-    [(empty? l) 0]
-    [(and (pos? l)
-          (cons? l)) (+ (first l)
-                        (checked-sum (rest l)))]
-    [else (error "invalid list, positive numbers only")])) ; #3 #!
+    [(pos? l) (aloa-sum l)]
+    [else (error "invalid list, positive numbers only")])) ; #3
+
+; #3 #!
+;(define (checked-sum l)
+;  (cond
+;    [(empty? l) 0]
+;    [(and (pos? l)
+;          (cons? l)) (+ (first l)
+;                        (checked-sum (rest l)))]
+;    [else (error "invalid list, positive numbers only")]))
 
 (check-error (checked-sum ALON4))
 (check-expect (checked-sum ALON0) 0)
 (check-expect (checked-sum ALON1) 1)
 (check-expect (checked-sum ALON2) 3)
 (check-expect (checked-sum ALON3) 6)
+
+
+
+
+;;; Exercise 140
+;;; ------------
+
+; A list of bools is one of:
+; - '()
+; - (cons Boolean List-of-bools)
+
+; List-of-bools -> Boolean
+; Takes a list and checks if all values are #true
+(define (all-true l)
+  (cond
+    [(empty? l) #true] ; #4 #!
+    [else (and (equal? (first l) #true)
+               (all-true (rest l)))]))
+
+(define LOB0 '())
+(define LOB1 (cons #true LOB0))
+(define LOB2 (cons #true LOB1))
+(define LOB3 (cons #true (cons #false '())))
+
+(check-expect (all-true LOB0) #true)
+(check-expect (all-true LOB1) #true)
+(check-expect (all-true LOB2) #true)
+(check-expect (all-true LOB3) #false)
+
+; List-of-bools -> Boolean
+; Takes a list and checks if they're of List-of-bools type
+(define (alob? l)
+  (cond
+    [(empty? l) #true] ; #5
+    [(cons? l) (and (boolean? (first l))
+               (alob? (rest l)))]))
+
+(define ALOB?0 '())
+(define ALOB?1 (cons 1 '()))
+(define ALOB?2 (cons #t (cons #f '())))
+(define ALOB?3 (cons #t (cons #f (cons "this" '()))))
+(define ALOB?4 (cons #t (cons #t (cons #t '()))))
+
+(check-expect (alob? ALOB?0) #t)
+(check-expect (alob? ALOB?1) #f)
+(check-expect (alob? ALOB?2) #t)
+(check-expect (alob? ALOB?3) #f)
+
+; Any -> Boolean
+; Check that each list item is a member of List-of-bools
+; Then run the all-true function
+(define (checked-all-true l)
+  (cond
+    [(alob? l) (all-true l)]
+    [else
+     (error "wrong type or not a list-of-bools type")]))
+
+(check-expect (checked-all-true ALOB?0) #t) ; #!
+(check-error (checked-all-true ALOB?1))
+(check-error (checked-all-true ALOB?3))
+(check-expect (checked-all-true ALOB?4) #t)
